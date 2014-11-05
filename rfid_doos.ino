3e187882a1c436e8425418da1ab95b0bae621ab5
Kurt Beheydt <kurt@postduif.be>
2014-11-05 20:31:02 +0100

    neopixels added

--- rfid_doos.ino
+++ rfid_doos.ino
@@ -1,6 +1,8 @@
 #include <LiquidCrystal.h>
 #include <SPI.h>
-#include <MFRC522.h>
+#include <MFRC522.h> // using library from https://github.com/miguelbalboa/rfid/
+#include <Servo.h> 
+#include <Adafruit_NeoPixel.h>
 
 // the pins in use by the LCD panel
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
@@ -19,23 +21,38 @@ MFRC522 mfrc522(SS_PIN, RST_PIN);
 unsigned int uid;
 String uid_txt;
 
+Servo myservo; 
+#define servoPin 1
+
+#define pixelPin 3
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(4, pixelPin, NEO_GRB + NEO_KHZ800);
+
 void setup() {
-  Serial.begin(9600);
   SPI.begin();
   
+  // mifare reader init
   mfrc522.PCD_Init();
   
+  // lcd screen init
   lcd.begin(16, 2);
   lcd.setCursor(0,0);
   lcd.print("Scan RFID");
+  
+  // servo init
+  myservo.attach(servoPin);
+  myservo.write(0);
+  
+  // neoPixelstrip init
+  strip.begin();
+  strip.show();
 }
 
 void loop() {
+  strip.Color(255, 0, 0);
   if (mfrc522.PICC_IsNewCardPresent()) {
     if (mfrc522.PICC_ReadCardSerial()) {
       lcd.setCursor(0,1);
       lcd.print("Card");
-      Serial.print("Card UID:");
       for (byte i = 0; i < mfrc522.uid.size; i++) {
         lcd.setCursor(i*2, 1);
         if (mfrc522.uid.uidByte[i] < 0x10) {
@@ -43,11 +60,8 @@ void loop() {
         }
         
         lcd.print(mfrc522.uid.uidByte[i], HEX);
-
-        Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
-        Serial.print(mfrc522.uid.uidByte[i], HEX);
+        myservo.write(50);
       } 
-      Serial.println();      
     }
   }
 }
